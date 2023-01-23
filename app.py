04c6e5c5f72b2b97ead21e695d19dab7757dd9be
Ameen Attar <35619352+AmeenAttar@users.noreply.github.com>
2023-01-23 16:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,24 +10,29 @@ openai.api_key = os.getenv("MY_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        age = request.form["age"]
-        user = request.form["user"]
-        prompt = "Explain" + user + "to a" + age + "year old"
+         recipient= request.form["recipient"]
+        recipientPosition= request.form["recipientPosition"]
+        sender= request.form["sender"]
+        ##senderPosition= request.form["senderPosition"]
+        reason = request.form["reason"]
+        prompts = "Write a structured corporate email from "+ sender+ ", to " + recipient + ", who is a " + recipientPosition+ " stating" + reason 
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(prompt),
-            temperature=0,
-            max_tokens=3000,
+            prompt=generate_prompt(prompts),
+            temperature=0.8,
+            max_tokens=4000,
         )
+        print("?????????????????????????????????????????????????????????????????????????????????????")
+        print (prompts)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(user):
+def generate_prompt(prompts):
     return """
 User: {}
-Explanation:""".format(
-        user.capitalize()
+Mail:""".format(
+        prompts.capitalize()
     )
+
